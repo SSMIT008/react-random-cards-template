3b8ae3823093189b5a7806b0ddd49dd0a1ee8296
Mr.Smit <stan.smit008@gmail.com>
2026-05-31 19:58:12 -0400

    Fix GitHub Pages routing with HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import HomePage from "./Layout/HomePage/HomePage";
 import PracticeBasics from "./Layout/Practice_Basics/PracticeBasics";
@@ -7,13 +7,13 @@ import PracticeHooks from "./Layout/Practice_Hooks/PracticeHooks";
 
 function App() {
     return (
-        <BrowserRouter basename="/react-random-cards-template">
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/PracticeBasics" element={<PracticeBasics />} />
                 <Route path="/PracticeHooks" element={<PracticeHooks />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
